c757cf74e955b345a753d393ffdca5ffbd277d9c
DHRUVA007 <48860908+DHRUVA007@users.noreply.github.com>
2021-01-18 21:04:02 +0530

    Update About.js

--- About.js
+++ About.js
@@ -7,10 +7,11 @@ const About=()=> {
              </div>
     <div class="content-two">
     hey ! This is Dhruva And if You want a Website Like this  ,Feel free to contect Me <h3>HERE is my FACEBOOK Profile</h3><br><hr>
-    <a class="button" href="https://www.facebook.com/dusttan.person"><h2>FACEBOOK</h2></a> <hr>
+    <a class="button" href="https://www.facebook.com/dusttan.person"><h2>FACEBOOK</h2></a> <hr><br>
 
     Open with Android browser And Boom you will get news related with Technology from All over the world
-    <br><br><br>
+    I have fetched two apis here .Sometimes, the video you are trying to play may not play sorry for that as because they are corrupted .
+<br><br><br>
     ~Web Developer
     </div>
    
