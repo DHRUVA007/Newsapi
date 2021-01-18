3e85fe603fbdcfc88c4fcec6805ca5ba62e020f0
DHRUVA007 <48860908+DHRUVA007@users.noreply.github.com>
2021-01-18 20:12:28 +0530

    Update About.js

--- About.js
+++ About.js
@@ -8,6 +8,8 @@ const About=()=> {
     <div class="content-two">
     hey ! This is Dhruva And if You want a Website Like this  ,Feel free to contect Me <h3>HERE is my FACEBOOK Profile</h3><br><hr>
     <a class="button" href="https://www.facebook.com/dusttan.person"><h2>FACEBOOK</h2></a> <hr>
+
+    Open with Android browser And Boom you will get news related with Technology from All over the world
     <br><br><br>
     ~Web Developer
     </div>
@@ -15,6 +17,7 @@ const About=()=> {
     `
     
 }
+
 
 
 
